04dcee88b72a0ce890cbd529284a7ee47e750d81
dirtmerchant <bert@bertbullough.com>
2021-09-23 17:02:13 +0000

    stores port number as an environment variable

--- server.go
+++ server.go
@@ -3,12 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./public")))
-	log.Print("Server starting at localhost:4444")
-	if err := http.ListenAndServe(":4444", nil); err != nil {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
+
+	log.Print("Server starting at localhost:" + port)
+
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
+
 }
